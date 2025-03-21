408f258ba3d427864f568d31f9dbd47b5ef39039
q100va <q100wa@gmail.com>
2025-03-21 10:19:14 -0400

    stable version 1

--- server/app.js
+++ server/app.js
@@ -4,20 +4,46 @@ const { json, urlencoded } = pkg;
 import { fileURLToPath } from 'url';
 import { join } from "path";
 import path from 'path';
-
-
+import sequelize from './database.js';
+/* import 'dotenv/config';
+import { config } from 'dotenv';
+config({ debug: true, override: true }); */
 
 const app = express();
 app.use(json({ limit: '50mb' }));
 app.use(urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
+//console.log(dotenv.config());
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
-console.log('__dirname');
-console.log(__dirname);
+//console.log('__dirname');
+//console.log(__dirname);
 global.__basedir = __dirname;
 app.use(express.static(join(__dirname, '../public')));
+
+//db connection postgres
+
+//postgres://<db_user>:<db_password>@127.0.0.1:5432/dev_db
+
+sequelize
+  .authenticate()
+  //.then(Country.sync())
+  //.then(Region.sync())
+  //.then(District.sync())
+  //.then(Locality.sync())
+  //.then(Contact.sync())
+  //.then(Address.sync())
+  //.then(SearchUser.sync())
+  //.then(Role.sync())
+ // .then(Operation.sync())
+  //.then(User.sync())
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.log('Unable to connect to the database:', err);
+  });
 
 // CORS
 
--- server/database.js
+++ server/database.js
@@ -1,4 +1,14 @@
 import Sequelize from 'sequelize';
+//import 'dotenv/config';
+
+import 'dotenv/config';
+/* import { config } from 'dotenv';
+config({ debug: true}); */
+
+console.log('process.env.DB_USERNAME');
+console.log(process.env.DB_USERNAME);
+console.log(process.env);
+//console.log(dotenv.config());
 
 const sequelize = new Sequelize('postgres://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_LINK);
 //const sequelize = new Sequelize('postgres://' + 'q100va' + ':' + '102Verbluda!' + '@' + 'localhost:5432/cards');
